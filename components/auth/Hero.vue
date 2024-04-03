ed6d86eddfa7f44b962520fd033b6f0210c057a1
Rodrigo Ochoa <roaloch@icloud.com>
2024-04-03 14:19:31 -0600

    FIX - UI Hover effect on hero features (login page)

--- components/auth/Hero.vue
+++ components/auth/Hero.vue
@@ -9,17 +9,17 @@
     </div>
     <div class="flex flex-col gap-4">
       <div
-        class="hover:border-primary dark:hover:border-primary cursor-pointer border-l-4 dark:border-gray-800"
+        class="hover:border-primary dark:hover:border-primary group cursor-pointer border-l-4 dark:border-gray-800"
       >
-        <h3 class="pl-4">
+        <h3 class="pl-4 transition-transform group-hover:translate-x-2">
           Generacion de playlist musicales<br />
           basadas en el reconocimiento de emociones
         </h3>
       </div>
       <div
-        class="hover:border-primary dark:hover:border-primary cursor-pointer border-l-4 dark:border-gray-800"
+        class="hover:border-primary dark:hover:border-primary group cursor-pointer border-l-4 dark:border-gray-800"
       >
-        <h3 class="pl-4">
+        <h3 class="pl-4 transition-transform group-hover:translate-x-2">
           Ganador del primer lugar del concurso<br />
           de Proyectos Modulares de la carrera<br />
           Ingeniería Informática 2023B - CUCEI
